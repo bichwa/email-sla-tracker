f0a3c1c16a74cd0748869a45daf402af1d084397
Erick Karani <rickarani@gmail.com>
2026-01-23 00:27:45 +0300

    Modified index.html and msal.js

--- src/lib/msal.js
+++ src/lib/msal.js
@@ -4,7 +4,7 @@ const msalConfig = {
     auth: {
         clientId: import.meta.env.VITE_MICROSOFT_CLIENT_ID,
         authority: `https://login.microsoftonline.com/${import.meta.env.VITE_MICROSOFT_TENANT_ID}`,
-        redirectUri: import.meta.env.VITE_MICROSOFT_REDIRECT_URI || window.location.origin + '/auth/callback',
+        redirectUri: window.location.origin,
     },
     cache: {
         cacheLocation: 'localStorage',
